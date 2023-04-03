8098bdb76335317078737b07b266bf33cb28401d
amtoine <stevan.antoine@gmail.com>
2023-04-03 18:41:29 +0200

    add description to my personal bindings

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -1,5 +1,5 @@
-vim.keymap.set('n', '<leader>so', ':source ~/.config/nvim/init.lua<CR>', { silent = true })
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true })
-vim.keymap.set('n', '<leader>t', ':terminal nu<CR>', { silent = true })
+vim.keymap.set('n', '<leader>so', ':source ~/.config/nvim/init.lua<CR>', { silent = true, desc = "[so]urce the config" })
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true, desc = "Remove the search [h]ighlight" })
+vim.keymap.set('n', '<leader>t', ':terminal nu<CR>', { silent = true, desc = "Open a [t]erminal with nushell" })
 vim.keymap.set('n', '<c-d>', '<c-d>zz', { silent = true })
 vim.keymap.set('n', '<c-u>', '<c-u>zz', { silent = true })
