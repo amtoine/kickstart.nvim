814fdf104c64b7ce0ec69e73dffbc65bbf75b120
amtoine <stevan.antoine@gmail.com>
2023-04-03 20:00:32 +0200

    add gf and pf to find more files

--- lua/custom/keybindings.lua
+++ lua/custom/keybindings.lua
@@ -3,3 +3,6 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { silent = true, desc = "Rem
 vim.keymap.set('n', '<leader>t', ':terminal nu<CR>', { silent = true, desc = "Open a [t]erminal with nushell" })
 vim.keymap.set('n', '<c-d>', '<c-d>zz', { silent = true })
 vim.keymap.set('n', '<c-u>', '<c-u>zz', { silent = true })
+
+vim.keymap.set('n', '<leader>pf', vim.cmd.Ex, { silent = true, desc = 'O[p]en the [f]ile explorer' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { silent = true, desc = 'Search for [g]it [f]files' })
