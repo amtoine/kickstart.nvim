121a01435bd5d41fca86acef97bfa1565ca5af66
amtoine <stevan.antoine@gmail.com>
2023-04-12 18:53:46 +0200

    add horizontal and vertical terminal bindings
    This commit removes the explicit dependency on `nushell` as the
    terminal shell.

--- lua/custom/map.lua
+++ lua/custom/map.lua
@@ -1,6 +1,9 @@
 vim.keymap.set("n", "<leader>so", ":source ~/.config/nvim/init.lua<CR>", { silent = true, desc = "[so]urce the config" })
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { silent = true, desc = "Remove the search [h]ighlight" })
-vim.keymap.set("n", "<leader>t", ":terminal nu<CR>", { silent = true, desc = "Open a [t]erminal with nushell" })
+
+vim.keymap.set("n", "<leader>tt", ":terminal<CR>", { silent = true, desc = "Open a [t]erminal" })
+vim.keymap.set("n", "<leader>th", ":split +terminal<CR>", { silent = true, desc = "Open a [t]erminal in a [h]orizontal split" })
+vim.keymap.set("n", "<leader>tv", ":vsplit +terminal<CR>", { silent = true, desc = "Open a [t]erminal in a [v]ertical split" })
 
 vim.keymap.set("n", "<leader>pf", vim.cmd.Ex, { silent = true, desc = "O[p]en the [f]ile explorer" })
 vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { silent = true, desc = "Search for [g]it [f]files" })
