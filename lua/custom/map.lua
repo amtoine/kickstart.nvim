e282d2dec69694479b43a0b1c27cd56097d07fb4
amtoine <stevan.antoine@gmail.com>
2023-04-14 18:30:25 +0200

    change the `nohl` binding from ` h` to ` nh`

--- lua/custom/map.lua
+++ lua/custom/map.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>so", ":source ~/.config/nvim/init.lua<CR>", { silent = true, desc = "[so]urce the config" })
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { silent = true, desc = "Remove the search [h]ighlight" })
+vim.keymap.set("n", "<leader>nh", ":nohlsearch<CR>", { silent = true, desc = "Remove the search ([n]o) [h]ighlight" })
 
 vim.keymap.set("n", "<leader>pf", vim.cmd.Ex, { silent = true, desc = "O[p]en the [f]ile explorer" })
 vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { silent = true, desc = "Search for [g]it [f]files" })
