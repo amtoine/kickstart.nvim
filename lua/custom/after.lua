9d5a0c799c20ac6d6bc2a2fca59c10c3a07bace5
amtoine <stevan.antoine@gmail.com>
2023-05-25 19:36:16 +0200

    remove the not-so-usefull `vim` and `api` variables

--- lua/custom/after.lua
+++ lua/custom/after.lua
@@ -1,17 +1,15 @@
-local vim = vim
-local api = vim.api
-local M = {}
+local M = {}  
 -- function to create a list of commands and convert them to autocommands
 -------- This function is taken from https://github.com/norcalli/nvim_utils
 function M.nvim_create_augroups(definitions)
     for group_name, definition in pairs(definitions) do
-        api.nvim_command('augroup '..group_name)
-        api.nvim_command('autocmd!')
+        vim.api.nvim_command('augroup '..group_name)
+        vim.api.nvim_command('autocmd!')
         for _, def in ipairs(definition) do
             local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
-            api.nvim_command(command)
+            vim.api.nvim_command(command)
         end
-        api.nvim_command('augroup END')
+        vim.api.nvim_command('augroup END')
     end
 end
 
