3f181c27232417821a7fee577d9c55e2c3bc6cb8
amtoine <stevan.antoine@gmail.com>
2023-06-17 12:52:22 +0200

    add a DAP config for C/C++/Rust
    inspired from https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#ccrust-via-lldb-vscode

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -6,9 +6,51 @@ return {
     "simrat39/rust-tools.nvim"
   },
   config = function()
+    local dap = require('dap')
     local dapui = require("dapui")
 
     dapui.setup()
+
+    dap.adapters.lldb = {
+      type = 'executable',
+      command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+      name = 'lldb'
+    }
+
+    local lldb_dap_config = {
+      name = 'Launch',
+      type = 'lldb',
+      request = 'launch',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = false,
+      args = {},
+    }
+    dap.configurations.cpp = { lldb_dap_config }
+    dap.configurations.c = { lldb_dap_config }
+
+    local rust_dap_config = lldb_dap_config
+    lldb_dap_config["initCommands"] = function()
+      local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+      local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+      local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+      local commands = {}
+      local file = io.open(commands_file, 'r')
+      if file then
+        for line in file:lines() do
+          table.insert(commands, line)
+        end
+        file:close()
+      end
+      table.insert(commands, 1, script_import)
+
+      return commands
+    end
+    dap.configurations.rust = { rust_dap_config }
 
     vim.api.nvim_set_keymap("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", { noremap = true })
     vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
