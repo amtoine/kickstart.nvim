a45c2338629239f24d4629e8399bce5163e75411
amtoine <stevan.antoine@gmail.com>
2023-08-06 11:27:04 +0200

    move all LaTeX plugins together

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,21 +22,6 @@ return {
   },
 
   {
-    "nvim-telescope/telescope-bibtex.nvim",
-    requires = {
-      { 'nvim-telescope/telescope.nvim' },
-    },
-    config = function()
-      local telescope = require("telescope")
-
-      telescope.load_extension("bibtex")
-
-      vim.keymap.set("n", "<leader>bbt", function() telescope.extensions.bibtex.bibtex {} end,
-        { silent = true, desc = "propose [b]i[bt]ex references for copy" })
-    end,
-  },
-
-  {
     "laytan/cloak.nvim",
     config = function()
       require('cloak').setup({
@@ -299,6 +284,21 @@ return {
     config = function()
       vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
     end
+  },
+
+  {
+    "nvim-telescope/telescope-bibtex.nvim",
+    requires = {
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      telescope.load_extension("bibtex")
+
+      vim.keymap.set("n", "<leader>bbt", function() telescope.extensions.bibtex.bibtex {} end,
+        { silent = true, desc = "propose [b]i[bt]ex references for copy" })
+    end,
   },
 
   { "lervag/vimtex" },
