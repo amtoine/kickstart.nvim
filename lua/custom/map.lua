b375e67c0ecba8dc5f57935059e187a8fde5da2d
amtoine <stevan.antoine@gmail.com>
2023-11-21 18:37:17 +0100

    add some descriptions to the keymap
    related to the quick and loc lists

--- lua/custom/map.lua
+++ lua/custom/map.lua
@@ -27,14 +27,14 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "cut to system clipb
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "get out of insert mode with control + C" })
 
 -- move in the code actions lists
-vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>")
-vim.keymap.set("n", "<leader>cc", "<cmd>cclose<CR>")
-vim.keymap.set("n", "<leader>cj", "<cmd>copen<CR><cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>ck", "<cmd>copen<CR><cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>lo", "<cmd>lopen<CR>")
-vim.keymap.set("n", "<leader>lc", "<cmd>lclose<CR>")
-vim.keymap.set("n", "<leader>lj", "<cmd>lopen<CR><cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>lk", "<cmd>lopen<CR><cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>", { desc = "open quick list" })
+vim.keymap.set("n", "<leader>cc", "<cmd>cclose<CR>", { desc = "close quick list" })
+vim.keymap.set("n", "<leader>cj", "<cmd>copen<CR><cmd>cnext<CR>zz", { desc = "next in quick list" })
+vim.keymap.set("n", "<leader>ck", "<cmd>copen<CR><cmd>cprev<CR>zz", { desc = "previous in quick list" })
+vim.keymap.set("n", "<leader>lo", "<cmd>lopen<CR>", { desc = "open loc list" })
+vim.keymap.set("n", "<leader>lc", "<cmd>lclose<CR>", { desc = "close loc list" })
+vim.keymap.set("n", "<leader>lj", "<cmd>lopen<CR><cmd>lnext<CR>zz", { desc = "next in loc list" })
+vim.keymap.set("n", "<leader>lk", "<cmd>lopen<CR><cmd>lprev<CR>zz", { desc = "previous in loc list" })
 
 -- misc
 vim.keymap.set("n", "Q", "<nop>", { desc = "do not do anything on Q" })
